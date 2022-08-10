9d87f8d15ec71b143410086f6fdedf0295a92fe6
Eugenio <carvalhoAlberto03@gmail.com>
2022-08-10 20:01:26 -0300

    feat: layer repository

---
+++ src/main/java/br/com/bank/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.bank.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.bank.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
